76e155901fe60c45b43f3b92d293d9120294d354
Tyler Durrdin <korbelz130@gmail.com>
2019-01-15 13:33:28 -0700

    The answer is 4 :P

---
+++ bomber.py
@@ -0,0 +1,27 @@
+# Comment section
+# bomber written by Korbelz
+# current scope: Airfield size required for bombers
+
+
+print ('*** This app is a Airfield size calc for level bombers ***')
+print ('*** Written by Korbelz ***')
+print ('*** Feedback/Bugs: Discord: Korbelz#3504 ***')
+input('Press ENTER to continue')
+
+print ('Examples: Sally 2205, Nell 1764, Betty 1764')
+print ('Examples: B-17 6800, A-20 2400, B-25 4000')
+
+i_loop = 0 
+
+while i_loop < 10:
+    print ('')
+    max_load = input("What is the max load of the level bomber? ")
+    max_load = int(max_load)
+
+    airfield_size = 4 + (max_load / 6500)
+    airfield_size = int(airfield_size)
+
+    print (f'This bomber required an airfield size of: {airfield_size}')
+    i_loop += 1 
+
+input('Press ENTER to exit')
