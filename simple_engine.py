d94b72a5ad00bbcd5ef751a8ac1b2134e0b18d9b
Tyler Durrdin <korbelz130@gmail.com>
2019-02-13 19:27:05 -0700

    simple_engine start

---
+++ simple_engine.py
@@ -0,0 +1,11 @@
+# Comment section
+# spoil written by Korbelz
+# current scope: simple engine numbers
+
+print ('*** This app is a simple engine calculator ***')
+print ('*** Written by Korbelz ***')
+print ('*** Feedback/Bugs: Discord: Korbelz#3504 ***')
+input('Press ENTER to continue')
+
+import csv 
+
